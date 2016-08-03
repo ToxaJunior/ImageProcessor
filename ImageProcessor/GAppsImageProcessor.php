619ea24a958347df55e9f6667b3ccd7856040599
Anton Hetmanov <anget@boboyan.net>
2016-08-03 16:36:16 +0300

    рабочая версия gapps

--- ImageProcessor/GAppsImageProcessor.php
+++ ImageProcessor/GAppsImageProcessor.php
@@ -13,15 +13,10 @@ use Google_Service_Drive_DriveFile;
 
 class GAppsImageProcessor implements ImageProcessor
 {
+    const DEVSTORAGE_URL = array('https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.apps.readonly');
 
-    const DEVSTORAGE_URL = \Google_Service_Drive::DRIVE_METADATA_READONLY;
-
-    private $projectName;
-    private $clientId;
-    private $clientSecret;
-    private $segmentName;
-    private $privateKey;
-    private $clientEmail;
+    private $service;
+    private $registry_email;
 
     /**
      * GAppsImageProcessor constructor.
@@ -31,95 +26,79 @@ class GAppsImageProcessor implements ImageProcessor
      * @param $segmentName
      * @param $privateKey
      */
-    public function __construct($projectName, $clientId, $clientSecret, $clientEmail, $segmentName, $privateKey)
+    public function __construct($projectName, $clientId, $clientEmail, $privateKey, $registry_email)
     {
-        $this->projectName = $projectName;
-        $this->clientId = $clientId;
-        $this->clientSecret = $clientSecret;
-        $this->segmentName = $segmentName;
-        $this->privateKey = $privateKey;
-        $this->clientEmail = $clientEmail;
+
+        $this->registry_email = $registry_email;
+        $client = new \Google_Client();
+
+        $client->setApplicationName($projectName);
+        $client->setClientId($clientId);
+        $client->setScopes(self::DEVSTORAGE_URL);
+
+        $privateKey = file_get_contents($privateKey); // получаем контент .p12 файла, который получили ранее
+        $client->setAssertionCredentials(new \Google_Auth_AssertionCredentials(
+            $clientEmail, // имеет вид 12345678910-fb2vu7ipertbg3t73shtu2m30b838756@developer.gserviceaccount.com, получили ранее
+            self::DEVSTORAGE_URL,
+            $privateKey
+        ));
+        $this->service = new \Google_Service_Drive($client);
     }
 
     public function save($param)
     {
-        $client = new \Google_Client();
 
-        $client->setApplicationName($this->projectName);
-        $client->setClientSecret('A2SVXIVVhjdJtJ86w9fIYC8-');
-      // $client->setRedirectUri('http://172.17.0.3/app_dev.php');
-        $client->setClientId('604919316416-1t3t22l7oo9techlo1c3tht0sr7i0uev.apps.googleusercontent.com');
-        $client->setScopes(self::DEVSTORAGE_URL);
-        $client->setAuthConfigFile('client_secret.json');
-        $client->setAccessType('offline');
+        $file = new Google_Service_Drive_DriveFile();
 
-//
-//        if (isset($_GET['code']) || (isset($_SESSION['access_token']) && $_SESSION['access_token'])) {
-//            if (isset($_GET['code'])) {
-//                $client->authenticate($_GET['code']);
-//                $_SESSION['access_token'] = $client->getAccessToken();
-//            } else
-//                $client->setAccessToken($_SESSION['access_token']);
+        $file->setTitle($param['origin']);
+        $file->setDescription('A test document');
+        $file->setMimeType('image/jpeg');
 
-//        $privateKey = file_get_contents($this->privateKey); // получаем контент .p12 файла, который получили ранее
-//        $client->setAssertionCredentials(new \Google_Auth_AssertionCredentials(
-//            $this->clientEmail, // имеет вид 12345678910-fb2vu7ipertbg3t73shtu2m30b838756@developer.gserviceaccount.com, получили ранее
-//            array(self::DEVSTORAGE_URL),
-//            $privateKey
-//        ));
-            $service = new \Google_Service_Drive($client);
-            $file = new Google_Service_Drive_DriveFile();
+        $data = file_get_contents($param['temp_path']);
 
-            $file->setTitle($param['origin']);
-            $file->setDescription('A test document');
-            $file->setMimeType('image/jpeg');
+        $createdFile = $this->service->files->insert($file, array(
+            'data' => $data,
+            'mimeType' => 'image/jpeg',
+            'uploadType' => 'multipart'
+        ));
 
-            $data = file_get_contents($param['temp_path']);
+        $createdFile->setPermissions($this->registry_email);
 
-            $res = $service->files->insert($file, array(
-                'data' => $data,
-                'mimeType' => 'image/jpeg',
-                'uploadType' => 'multipart'
-            ));
-           return $res;
-        }
-   // }
+        $permission = new \Google_Service_Drive_Permission();
+        $permission->setType('anyone');
+        $permission->setRole('reader');
+        $permission->setWithLink(true);
+        $permission->setValue(null);
 
-//        $object = new \Google_Service_Storage_StorageObject();
-//        $object->setName($param['origin']); // имя файла в bucket(на диске google)
-//        $res = $objects->insert(
-//            $this->segmentName, $object, array(
-//                'data' => file_get_contents($param['temp_path']), // контент файла
-//                'uploadType' => 'media',
-//                'predefinedAcl' => 'publicread' // присваиваем предопределенный ACL public-read
-//            )
-//        );
-//        $request = new Google_HttpRequest($object['selfLink']);
-//        $response = $this->client->getIo()->authenticatedRequest($request);
-    //       var_dump($response);die();
-//        return $res;
+        $this->service->permissions->insert($createdFile->getId(), $permission);
 
+        return $createdFile->getId();
+    }
 
     public function delete($fileId)
     {
-        // TODO: Implement delete() method.
+        if($this->service->files->delete($fileId))
+            return true;
+        return false;
     }
 
     public function show($fileId)
     {
-        $client = new \Google_Client();
+        $file = $this->service->files->get($fileId);
+        return str_replace("&export=download", "", $file->webContentLink);
 
-        $client->setApplicationName($this->projectName);
-        $client->setClientId($this->clientId);
-        $client->setScopes(self::DEVSTORAGE_URL);
-        $privateKey = file_get_contents($this->privateKey); // получаем контент .p12 файла, который получили ранее
-        $client->setAssertionCredentials(new \Google_Auth_AssertionCredentials(
-            $this->clientEmail, // имеет вид 12345678910-fb2vu7ipertbg3t73shtu2m30b838756@developer.gserviceaccount.com, получили ранее
-            array(self::DEVSTORAGE_URL),
-            $privateKey
-        ));
-
-        echo "<img src='https://drive.google.com/file/d/0B99pTgxtw6TUMHkxNTVHOFNEMkE/view?usp=drivesdk'>";
-        die();
     }
+//    public function getListResourses(){
+//
+//        $listObjects = $this->service->files->listFiles()->getItems();
+//
+//        $list = array();
+//        $i = 0;
+//        foreach ($listObjects as $listObject){
+//
+//            $list[$i] = $listObject->id;
+//            $i++;
+//        }
+//        return $list;
+//    }
 }
